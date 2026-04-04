353c772fe06c97292228e6cc16f49580d84c86d6
Albert Braimi <albertbraimi@gmail.com>
2026-04-04 16:10:55 +0200

    change or add variables
    Time: 30
    Status: DONE

--- git-log.sh
+++ git-log.sh
@@ -14,28 +14,30 @@ if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     exit 1
 fi
 
-// ask for who to make the log
+# ask for who to make the log
+author=$(git shortlog -sn | awk '{ $1=""; print $0 }' | sed 's/^[ ]*//' | fzf --prompt=" > ")
 
 
 
+declare lastdate
 
-declare LASTDATE
+for hash in $(git rev-list --reverse --author=$author HEAD); do
+    currentdate=$(git show -s --format=%cd --date=format:'%d.%m.%Y' $hash)
+    msg=$(git show -s --format=%s $hash)
+    time=$(git show -s --format="%(trailers:key=Time,valueonly)" $hash | tr -d '\n')
+    status=$(git show -s --format="%(trailers:key=Status,valueonly)" $hash | tr -d '\n')
 
-for hash in $(git rev-list --reverse HEAD); do
-    CURRENTDATE=$(git show -s --format=%cd --date=format:'%d.%m.%Y' $hash)
-    MSG=$(git show -s --format=%s $hash)
-
-    if [ "$CURRENTDATE" != "$LASTDATE" ]; then
-        LASTDATE="$CURRENTDATE"
+    if [ "$currentdate" != "$lastdate" ]; then
+        lastdate="$currentdate"
 
         echo "" >> "$OUTPUT"
         echo "" >> "$OUTPUT"
-        echo "# $CURRENTDATE" >> "$OUTPUT"
+        echo "# $currentdate" >> "$OUTPUT"
         echo "| Status | Time | Message |" >> "$OUTPUT"
         echo "| --- | --- | --- |" >> "$OUTPUT"
     fi
 
-    git show -s --format="| %(trailers:key=Status,valueonly) | %(trailers:key=Time,valueonly) | %s |" $hash >> "$OUTPUT"
+    git show -s --format="| $status | $time | %s |" $hash >> "$OUTPUT"
 
 done
 
