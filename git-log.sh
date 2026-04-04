c421518068a5fbf7f358eef92bfc50e237b09cb3
Albert Braimi <albertbraimi@gmail.com>
2026-04-04 11:35:38 +0200

    add the trailers support
    Time: 20m
    Status: DONE

--- git-log.sh
+++ git-log.sh
@@ -14,27 +14,29 @@ if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     exit 1
 fi
 
-
+// ask for who to make the log
 
 
 
 
 declare LASTDATE
 
-for hash in $(git rev-list HEAD); do
+for hash in $(git rev-list --reverse HEAD); do
     CURRENTDATE=$(git show -s --format=%cd --date=format:'%d.%m.%Y' $hash)
     MSG=$(git show -s --format=%s $hash)
-    
+
     if [ "$CURRENTDATE" != "$LASTDATE" ]; then
         LASTDATE="$CURRENTDATE"
-        
+
         echo "" >> "$OUTPUT"
         echo "" >> "$OUTPUT"
         echo "# $CURRENTDATE" >> "$OUTPUT"
+        echo "| Status | Time | Message |" >> "$OUTPUT"
+        echo "| --- | --- | --- |" >> "$OUTPUT"
     fi
-    
-    git show -s --format="|  |  | %an | %s |" $hash >> "$OUTPUT"
-    
+
+    git show -s --format="| %(trailers:key=Status,valueonly) | %(trailers:key=Time,valueonly) | %s |" $hash >> "$OUTPUT"
+
 done
 
 
